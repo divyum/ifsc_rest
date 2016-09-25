39e9e3aca41671bada5efb32e48cd0963c29906a
divyum <divyum@browserstack.com>
2016-09-26 03:17:18 +0530

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,56 @@
+require 'json'
+
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  def index
+  end
+
+  def get_branch_details
+    if params[:ifsc].nil? || params[:ifsc].empty?
+      error = "Invalid arguments!"
+    else
+      error = ""
+      branch = Branch.where(:ifsc => params[:ifsc].upcase)
+      error = "No such IFSC exists" if branch.empty?
+    end
+
+  	if error.empty?
+  		bank = branch[0].bank.name
+  		render json: {bank => branch}
+  	else
+  		render json: {error: error}
+  	end
+  end
+
+  def get_city_branches
+    if params[:name].nil? || params[:city].nil? || params[:name].empty? || params[:city].empty?
+      error = "Invalid arguments!"
+    else
+      error = ""
+    	bank = Bank.where(:name => params[:name].upcase)
+    	if bank.empty?
+    		error = "No such bank. Please check again"
+    	else
+    		branches = bank[0].branches.where(:city => params[:city].upcase)
+    		if branches.empty?
+    			error = "No branches found of the bank."
+    		else
+    			branches_json = []
+    			branches.each do |branch|
+    				branches_json << branch
+    			end
+    		end
+      end
+  	end
+
+  	if error.empty?
+  		render json: { bank[0].name => branches_json }
+  	else
+  		render json: {error: error}
+  	end
+  end
+
+end
---
+++ app/views/branches/_branch.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! branch, :id, :ifsc, :bank_id, :branch, :address, :city, :district, :state, :created_at, :updated_at
+json.url branch_url(branch, format: :json)
